23bdaab88944a7927db75f663bf1bd96ad60edc3
davoodfox <trstdavood@gmail.com>
2023-10-12 12:54:40 +0330

    feat:adding demo user

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,19 @@
-import { SignIn } from '@clerk/nextjs'
+import { Text } from "@/components/ui/Text";
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col gap-6 justify-center items-center h-screen">
+      <div className="flex justify-center items-center gap-4">
+        <Text variant="large/semibold">
+          <h3>Demo:</h3>
+        </Text>
+        <div className="flex flex-col">
+          <span>Username: demo</span>
+          <span>Password: demopass</span>
+        </div>
+      </div>
       <SignIn />
     </div>
-  )
+  );
 }
